6bf551a8e3521b4b635d002626b9e5e8d4e94a0f
Amit Chand <amtchand22@gmail.com>
2021-05-16 16:19:25 +0545

    Code Share

---
+++ frontend/src/component/data.js
@@ -0,0 +1,76 @@
+import first from '../images/first.jpg';
+import second from '../images/second.jpg';
+import third from '../images/third.jpg';
+import fourth from '../images/third.jpg';
+import fifth from '../images/third.jpg';
+
+
+
+
+  const data={
+    products:[
+        {
+            _id:'1',
+            name:'Formal Shirts',
+            image:first,
+            price:120,
+            brand:'Nike',
+            rating: 3,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Shirts',
+            image:fifth,
+            price:300,
+            brand:'Adidas',
+            rating: 4,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste Shirts',
+            image:second,
+            price:500,
+            brand:'Lacoste',
+            rating: 5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Pants',
+            image:fourth,
+            price:170,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Pants',
+            image:first,
+            price:90,
+            brand:'Puma',
+            rating: 3.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Fit Shirts',
+            image:second,
+            price:55,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews:10,
+            description:'High quality product'
+        }
+    ],
+}
+
+
+export default data;
